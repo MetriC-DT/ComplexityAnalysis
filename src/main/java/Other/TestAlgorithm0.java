29fa3da3f955f6e5839c91a7c7b1cbb86b0c9d4a
Derick Tseng <derickboss1@gmail.com>
2020-04-06 00:10:18 -0700

    make mergesort in place

--- src/main/java/Other/TestAlgorithm0.java
+++ src/main/java/Other/TestAlgorithm0.java
@@ -16,8 +16,11 @@ public class TestAlgorithm0 implements Complexity {
         return this.getClass().getSimpleName();
     }
 
+    /**
+     * Example of a Theta(n) complexity algorithm
+     * @param n the complexity
+     */
     private void i(double n) {
-        // example of a theta(n) algorithm
         if (n < 1) return;
         for (int i = 0; i < n; i++);
         i((999 * n) / 1000);
--- src/main/java/Sort/MergeSort.java
+++ src/main/java/Sort/MergeSort.java
@@ -1,56 +1,40 @@
 package Sort;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class MergeSort extends Sort {
     @Override
     public void sort(int[] arr) {
-        mergeSort(arr, arr.length);
+        mergeSort(arr, 0, arr.length);
     }
 
-    private static void mergeSort(int[] arr, int len) {
-        if (len < 2) {
+    /**
+     * applies mergesort to an array from index low to high
+     * @param array array to mergesort
+     * @param low lower bound
+     * @param high top bound
+     */
+    private void mergeSort(int[] array, int low, int high) {
+        if (high - low <= 1) {
             return;
-        }
+        } else {
+            int mid = (high + low) / 2;
+            mergeSort(array, low, mid);
+            mergeSort(array, mid, high);
 
-        int mid = len / 2;
-        int [] left_arr = new int[mid];
-        int [] right_arr = new int[len-mid];
-        int k = 0;
-        for(int i = 0; i < len; ++i){
-            if(i < mid){
-                left_arr[i] = arr[i];
+            int lIndex = low;
+            int hIndex = mid;
+            while (lIndex < mid && hIndex < high) {
+                if (array[hIndex] < array[lIndex]) {
+                    int tmp = array[hIndex];
+                    System.arraycopy(array, lIndex, array, lIndex + 1, hIndex - lIndex);
+                    array[lIndex] = tmp;
+                    hIndex++;
+                    lIndex++;
+                    mid++;
+                } else {
+                    lIndex++;
+                }
             }
-            else{
-                right_arr[k] = arr[i];
-                k = k+1;
-            }
-        }
-        mergeSort(left_arr,mid);
-        mergeSort(right_arr,len-mid);
-        merge(left_arr,right_arr,arr,mid,len-mid);
-    }
-
-    private static void merge(int[] left_arr,int[] right_arr, int[] arr,int left_size, int right_size){
-        int i = 0, l = 0, r = 0;
-        //The while loops check the conditions for merging
-        while(l < left_size && r < right_size){
-
-            if(left_arr[l] < right_arr[r]){
-                arr[i++] = left_arr[l++];
-            }
-            else{
-                arr[i++] = right_arr[r++];
-            }
-        }
-        while(l < left_size){
-            arr[i++] = left_arr[l++];
-        }
-        while(r < right_size){
-            arr[i++] = right_arr[r++];
         }
     }
-
 
 }
